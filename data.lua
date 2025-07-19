373632a309a345ab3dfd7e9ff9d9afac57baf71f
Puzz1ebox <Raymond.Bailey.Box1@gmail.com>
2025-07-19 14:11:09 -0400

    Native button compat.

--- data.lua
+++ data.lua
@@ -3,3 +3,8 @@ require("__Quality-Plus-Plus__.prototypes.quality")
 
 local legendary = data.raw.quality["legendary"]
 legendary.next_probability = 0.1
+
+if data.raw["utility-constants"].default.quality_selector_dropdown_threshold then
+    data.raw["utility-constants"].default.quality_selector_dropdown_threshold = 18 -- Covers all Quality++ qualities and shiny variants if they are unhidden
+end
+
